546628658582949b4378e0cfb525ef12a16ceb91
nickmancari <nickjmancari@gmail.com>
2021-08-24 21:45:06 +0000

    removed debugging println

--- src/runner.rs
+++ src/runner.rs
@@ -2,6 +2,6 @@ use std::process::Command;
 
 pub fn run_command(command: String, parameter: String) {
 
-    println!("{} {}", command, parameter); //For debugging
+//    println!("{} {}", command, parameter); //For debugging
     Command::new(command).arg(parameter).status().expect("Run Failed!");
 }
