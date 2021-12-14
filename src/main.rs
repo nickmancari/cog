fe73ec34eafe62b771b4aa7c93b8478150c1d4e6
Nick Mancari <nickjmancari@gmail.com>
2021-12-14 16:11:04 +0000

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod handler;
 
 fn main() {
     let mut arguments = std::env::args().skip(1);
-    let value = arguments.next().expect("Value Not Provided!");
+    let value = arguments.next().unwrap_or("".to_string());
 
     let (m, p) = handler::match_input(&value);
     runner::run_command(m, p)
